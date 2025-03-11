6aa9f3dca74af84592bea9816ccdb06926025394
Bobby Brockway <bbrockway@mintel.com>
2025-03-11 12:04:45 +0000

    remove old versions

--- install-binaries.sh
+++ install-binaries.sh
@@ -11,9 +11,3 @@ grep -vP '(^terraform\s|\sterraform$)' tf-tool-versions > "$HOME/.tool-versions"
 # shellcheck disable=SC2016
 grep -E "^#asdf:" "$HOME/.tool-versions" | cut -d':' -f2- | tr '\n' '\0' | xargs -0 -n1 -Icmd -- sh -c '$HOME/.asdf/bin/asdf cmd'
 "$HOME/.asdf/bin/asdf" install
-
-# For backwards compatibility:
-"$HOME/.asdf/bin/asdf" install awscli 2.11.8
-"$HOME/.asdf/bin/asdf" install terraform-docs 0.16.0
-"$HOME/.asdf/bin/asdf" install tflint 0.34.1
-"$HOME/.asdf/bin/asdf" install tfsec 1.15.4
